fbf93cd02a1ab33cd4fe635d36ac12860e109248
Joydeep Roychowdhury <joydeepbeyondsky86@gmail.com>
2022-01-12 22:33:44 +0530

    Changes for helm chart

--- .db-creds/base64.sh
+++ .db-creds/base64.sh
@@ -1,4 +1,4 @@
 echo "complete_postgresql" | base64 | ( read dbuser; echo "DBUSER: $dbuser"; )
 echo "YourPwdShouldBeLongAndSecure!" | base64 | ( read dbpass; echo "DBPASS: $dbpass"; )
 echo "olien.colbewxzq7jv.us-east-1.rds.amazonaws.com" | base64 | ( read dbhost; echo "DBHOST: $dbhost"; )
-echo "completePostgresql" | base64 | ( read dbname; echo "DBNAME: $dbname"; )
+echo "olien" | base64 | ( read dbname; echo "DBNAME: $dbname"; )
